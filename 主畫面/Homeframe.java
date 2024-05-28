c51f0f797a61f07192e3b9baa76523d0963beebf
richrich777 <rich20040101@gmail.com>
2024-05-28 11:34:05 +0800

    bgm音量設定

--- 主畫面/Homeframe.java
+++ 主畫面/Homeframe.java
@@ -2,7 +2,6 @@ package test.frame;
 import javax.swing.*;
 
 import test.frame.Panel1;
-import test.frame.SettingWindow;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -21,11 +20,11 @@ public class Homeframe extends JFrame
     private Panel1 panel;
     private Clip clip;
     private JLayeredPane layeredPane;
+    private FloatControl volumeControl;
     private boolean isSettingWindowOpen = false;
 
     Homeframe(String text)
     {
-        playBackgroundMusic("music.wav");
         this.setVisible(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,10 +64,77 @@ public class Homeframe extends JFrame
         button2.setBounds(this.getWidth() - 85, 0, 75, 40);
         button2.addActionListener(new myActionListener());
         layeredPane.add(button2, JLayeredPane.PALETTE_LAYER);
-
         this.add(layeredPane);
+
+        playBackgroundMusic("music.wav");
     }
 
+    private class SettingWindow extends JFrame
+    {
+        private JSlider slider;
+        private JLabel label1;
+        private JPanel panel;
+        private JCheckBox checkBox1;
+    
+        public SettingWindow()
+        {
+            this.setSize(400,400);
+            this.setLayout(new GridLayout(8,1));
+            this.setResizable(false);
+            // 文字欄位
+            label1 = new JLabel("BGM音量大小");
+            label1.setFont(new Font(null, Font.PLAIN, 20));
+            label1.setLayout(new FlowLayout(FlowLayout.RIGHT));
+            
+            // 開關
+            checkBox1 = new JCheckBox("開啟");
+            checkBox1.setSelected(true);
+            checkBox1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) 
+                {
+                    if (checkBox1.isSelected()) 
+                    {
+                        if (clip != null && !clip.isRunning()) 
+                        {
+                            clip.start();
+                        }
+                        slider.setEnabled(true);
+                    } 
+                    else 
+                    {
+                        if (clip != null && clip.isRunning()) 
+                        {
+                            clip.stop();
+                            slider.setEnabled(false);
+                        }
+                    }
+                }
+            });
+            panel = new JPanel();
+            panel.add(label1);
+            panel.add(checkBox1);
+            this.add(panel);
+            
+            slider = new JSlider(0, 100, 50);
+            slider.setMajorTickSpacing(20);
+            slider.setMinorTickSpacing(5); 
+            slider.setPaintTicks(true);
+            slider.setPaintLabels(true);
+            slider.addChangeListener(e -> {
+                if (clip != null && clip.isOpen()) {
+                    volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                    int value = slider.getValue();
+                    float dB = (float) (Math.log(value / 100.0) / Math.log(10.0) * 20.0);
+                    volumeControl.setValue(dB);
+                }
+            });
+            this.add(slider);
+
+            this.setVisible(true);
+        }
+    }
+    
     private void playBackgroundMusic(String filePath) 
     {
         try 
@@ -85,6 +151,7 @@ public class Homeframe extends JFrame
             e.printStackTrace();
         }
     }
+    
     public class myActionListener implements ActionListener
     {
         @Override
